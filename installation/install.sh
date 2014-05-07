500f6ab3256106e2e8ea9fbf58e26d4361b28404
alangrafu <alvaro@graves.cl>
2014-05-06 23:58:51 -0700

    Fixed basic settings.json at installation script

--- installation/install.sh
+++ installation/install.sh
@@ -74,7 +74,7 @@ echo " 		\"dbpedia\": \"http://dbpedia.org/sparql\"" >> $SETTINGS
 echo " 	}," >> $SETTINGS
 echo " 	\"host\": \"0.0.0.0\"," >> $SETTINGS
 echo " 	\"port\": $PORT", >> $SETTINGS
-echo " 	\"flod\": { #Here you can add custom variables", >> $SETTINGS
+echo " 	\"flod\": { >> $SETTINGS
 echo " 	  \"title\": \"FLOD\"" >> $SETTINGS
 echo " 	}," >> $SETTINGS
 echo " 	\"secret\": \"$APPSECRET\"", >> $SETTINGS
