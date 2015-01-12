623087eb3e2b90d7beec5dcb47d9b683df7f0be4
alangrafu <alvaro@graves.cl>
2015-01-12 00:10:53 -0800

    fixing install script

--- installation/install.sh
+++ installation/install.sh
@@ -176,10 +176,10 @@ cp installation/users.ttl .
 echo "Writing start.sh"
 echo "#!/bin/bash" > start.sh
 echo "source $VE_DIR/bin/activate" >> start.sh
-echo "HOST=\$(cat components/settings.json |python -c 'import json,sys;obj=json.load(sys.stdin);print obj["host"]')" >> start.sh
-echo "PORT=\$(cat components/settings.json |python -c 'import json,sys;obj=json.load(sys.stdin);print obj["port"]')" >> start.sh
-echo "echo Launching FLOD on $HOST:$PORT >&2" >> start.sh
-echo "uwsgi --http $HOST:$PORT -wflodserver:app --master --workers=2 --threads=10" >> start.sh
+echo "HOST=\$(cat components/settings.json |python -c 'import json,sys;obj=json.load(sys.stdin);print obj[\"host\"]')" >> start.sh
+echo "PORT=\$(cat components/settings.json |python -c 'import json,sys;obj=json.load(sys.stdin);print obj[\"port\"]')" >> start.sh
+echo "echo Launching FLOD on \$HOST:\$PORT >&2" >> start.sh
+echo "uwsgi --http \$HOST:\$PORT -wflodserver:app --master --workers=2 --threads=10" >> start.sh
 echo "deactivate" >> start.sh
 
 deactivate
